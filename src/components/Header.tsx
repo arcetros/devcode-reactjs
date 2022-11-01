3f3595c3cb988b48279258719d204dd7b461043c
arcetros <75669794+arcetros@users.noreply.github.com>
2022-11-02 00:59:28 +0800

    feat(dashboard): add EmptyDashboard component
    feat: add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Header: React.FunctionComponent = () => {
+  return (
+    <nav className="bg-[#16ABF8] w-full h-[105px]">
+      <div className="max-w-4xl flex h-full items-center m-auto">
+        <h1 className="font-bold text-2xl text-white">TO DO LIST APP</h1>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
