143c085f22ea0fec13988d260da13b7581e0eb08
arcetros <75669794+arcetros@users.noreply.github.com>
2022-11-02 22:19:19 +0800

    chore: add header cy tag

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,9 @@ const Header: React.FunctionComponent = () => {
   return (
     <header data-cy="header-background" className="bg-[#16ABF8] w-full h-[105px]">
       <div className="max-w-[1000px] flex h-full items-center m-auto">
-        <h1 className="font-bold text-2xl text-white">TO DO LIST APP</h1>
+        <h1 data-cy="header-title" className="font-bold text-2xl text-white">
+          TO DO LIST APP
+        </h1>
       </div>
     </header>
   )
